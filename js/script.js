be734de08684e83c01758a91849c334b5ce22cd9
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-05-09 17:27:23 -0500

    adds cursor imgage and implements in css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', {
+
+    // document.getElementById("container").onmousemove = function(){
+    //     document.getElementsByClassName("canvasjs-chart-canvas")[1].style.cursor = "pointer";
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+})
